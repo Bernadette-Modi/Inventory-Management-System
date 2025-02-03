b3e02786566f6d608b3992f12190046b8f3b0ed8
bernadette-modi <berniemain353@gmail.com>
2025-02-03 16:00:16 +0300

    Added a function that adds item to the inventory and increase it's quantity if the item already exits.

--- inventoryms.py
+++ inventoryms.py
@@ -1,3 +1,11 @@
 class Inventory:
     def __init__(self):
         self.items = {}
+
+    def add_items(self, name, quantity, price):
+        if name in self.items:
+            self.items[name]['quantity'] += quantity
+        else:
+            self.items[name] = {'quantity': quantity, 'price': price}
+            print(f"{quantity} of {name} added to inventory.")
+            
