b05cce46c3b332641a70913533954c05056aa809
bernadette-modi <berniemain353@gmail.com>
2025-02-03 22:17:43 +0300

    Added a def function that updates an existing item in the inventory and that updates the quantity and price if the item is found.

--- inventoryms.py
+++ inventoryms.py
@@ -8,4 +8,10 @@ class Inventory:
         else:
             self.items[name] = {'quantity': quantity, 'price': price}
             print(f"{quantity} of {name} added to inventory.")
-            
+
+    def update_item(self, name, quantity, price):
+        if name in self.items:
+            self.items[name] = {'quantity': quantity, 'price': price}
+            print(f"{name} updated successfully.")
+        else:
+            print(f"{name} not fount in inventory.")
